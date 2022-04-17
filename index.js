d0dae15c5b790178adc3bd245f0489e8d9b31f70
Abzed <ibraabzed@gmail.com>
2022-04-17 11:22:37 +0300

    commit tree ee54439214f6e96ac8edda38253f81fb0f9c42b2

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Your code here
+function moveDodgerLeft(){
+    let dodger = document.getElementById('dodger')
+    const leftNumbers = dodger.style.left.replace('px', '')
+    const left = parseInt(leftNumbers, 10)
+    if(left > 0){
+        dodger.style.left = `${left - 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e){
+    if(e.key == "ArrowLeft"){
+        moveDodgerLeft();
+    }
+})
+
+function moveDodgerRight(){
+    let dodger = document.getElementById('dodger')
+    var leftNumbers = dodger.style.left.replace('px', '')
+    var left = parseInt(leftNumbers, 10)
+
+    if(left > 0){
+        dodger.style.left = `${left + 1}px`
+    }
+}
+
+document.addEventListener('keydown', function(e){
+    // console.log(e);
+    if(e.key == 'ArrowRight'){
+        moveDodgerRight();
+    }
+})
+
+
+
+
+
+
+
+
+
+
+
+
